73d4d748ca11ba2029c6590e4a5c20aae641779a
Pascal Tung <tung.pascal@gmail.com>
2017-02-21 15:20:12 +0100

    add run capture btn

--- src/waveconverter_gui.py
+++ src/waveconverter_gui.py
@@ -18,6 +18,7 @@ from statEngine import buildStatStrings
 from collections import Counter
 import numpy as np
 import webbrowser
+from rawCaptureThread import RawCaptureThread
 
 # for plotting baseband
 try:
@@ -43,7 +44,8 @@ except:
 
 
 class TopWindow:
- 
+
+    runBtn = False
     spinButtonPressed = -1
     
     def on_window1_destroy(self, object, data=None):
@@ -327,7 +329,7 @@ class TopWindow:
             
         if txNum < len(wcv.txList):
             wcv.txNum = txNum
-            self.drawBasebandPlot(wcv.txList[wcv.txNum].waveformData, 
+            self.drawBasebandPlot(wcv.txList[wcv.txNum].waveformData,
                                   wcv.tMin, wcv.tMax, wcv.basebandSampleRate)
         else:
             print "Reached end of transmission list"
@@ -659,7 +661,6 @@ class TopWindow:
         self.axis.set_xlabel('time (ms)') # replace with time unit
         self.axis.set_ylabel('Baseband Signal') # replace with ???
         self.canvas.draw()
-        self.canvas0.draw()
         self.canvas1.draw()
         self.canvas2.draw()
         
@@ -1026,25 +1027,20 @@ class TopWindow:
         self.axis.set_xlabel('time (ms)')
         self.axis.set_ylabel('Baseband Signal')
         self.canvas = FigureCanvas(self.figure)  # a Gtk.DrawingArea
-        self.canvas0 = FigureCanvas(self.figure)  # a Gtk.DrawingArea
         self.canvas1 = FigureCanvas(self.figure)
         self.canvas2 = FigureCanvas(self.figure)
         self.canvas.set_size_request(400,40)
-        self.canvas0.set_size_request(400,40)
         self.canvas1.set_size_request(400,40)
         self.canvas2.set_size_request(400,40)
         
         # each canvas should be identical; assign to the widgets on 3 tabs
         self.plotWidget = self.builder.get_object("basebandPlot")
-        self.plotWidget0 = self.builder.get_object("basebandPlot0")
         self.plotWidget1 = self.builder.get_object("basebandPlot1")
         self.plotWidget2 = self.builder.get_object("basebandPlot2")
         self.plotWidget.add_with_viewport(self.canvas)
-        self.plotWidget0.add_with_viewport(self.canvas0)
         self.plotWidget1.add_with_viewport(self.canvas1)
         self.plotWidget2.add_with_viewport(self.canvas2)
         self.plotWidget.show_all()
-        self.plotWidget0.show_all()
         self.plotWidget1.show_all()
         self.plotWidget2.show_all()
         
@@ -1090,4 +1086,16 @@ class TopWindow:
         self.populateProtocolToGui(protocol)
 
     def on_runCapture_clicked(self, button, data=None):
-        os.system("python ../../rawcapture/rawcapture.py -f 101900000  -s 500000 -o output.iq")
+        t = RawCaptureThread()
+        if self.runBtn is False:
+            self.runBtn = True
+            print self.getIntFromEntry("entry1")
+            print self.getIntFromEntry("entry2")
+            print self.getStringFromEntry("entry3")
+            t.start()
+            button.set_label("Stop Capture")
+        else:
+            self.runBtn = False
+            button.set_label("Start Capture")
+            t.stop()
+            os.system("ps aux | grep ../../rawcapture/rawcapture.py | grep -v grep | awk '{print $2}' | xargs -n1 kill")
